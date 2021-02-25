16eb36a15cd4437d4f77faa2bc38713adc743754
Marek <mareksikka@gmail.com>
2021-02-25 17:01:14 +0200

    Where is my car created

---
+++ WhereIsMyCar/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhereIsMyCar
+{
+    class Car
+    {
+        //The car has a mark name(string), model name(string), registration number(string, no longer than six characters), color(string), odometer(int) and fuel tank(int)
+        private string _markName;
+        private string _modelName;
+        private string _regNumber;
+        private string _color;
+        private int _odometer = 0;
+        private int _fueltank = 60;
+        public Car(string markName, string modelName, string regNumber, string color)
+        {
+            _markName = markName;
+            _modelName = modelName;
+            if (regNumber.Length>6) {
+                throw new ArgumentOutOfRangeException($"Registration number cannot be more than 6 characters. Current: {regNumber}");  
+            }
+            _regNumber = regNumber;
+            _color = color;
+        }
+        public string Mark { get { return _markName; } }
+        public string Model { get { return _modelName; } }
+        public string Registration { get { return _regNumber; } }
+        public string Color { get { return _color; } }
+        public int Odometer { get { return _odometer; } }
+        public int FuelTank { get { return _fueltank; } }
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Mark: {_markName}\nModel: {_modelName}\nRegistration: {_regNumber}\nColor: {_color}\nOdometer: {_odometer}\nFueltank: {_fueltank}");
+        }
+        public void Drive()
+        {
+            Console.WriteLine("wroom-wroom");
+            _odometer += 100;
+            _fueltank -= 5;
+        }
+        public void DriveEmpty()
+        {
+            do
+            {
+                Drive();
+
+            } while (_fueltank > 0);
+        }
+    }
+}
---
+++ WhereIsMyCar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WhereIsMyCar
+{
+    class Program
+    {
+        //        Create a class Car
+        //The car has a mark name(string), model name(string), registration number(string, no longer than six characters), color(string), odometer(int) and fuel tank(int)
+        //The odometer value equals 0 by default.
+        //The fuel tank value by default is 60.
+        //No car characteristics can be changed after the car object has been created.*
+        //All the car characteristics can be accessed using ‘.’ (E.g.newCar.Model);
+        //The car can drive.Every lap the car passes increases the value of the odometer by 100 and decreases the value of the fuel tank by 5 .
+      
+   //     Part 2
+//Write a program that allows you to create a car object and makes the car drive until the fuel tank is empty(equals 0). When the fuel tank value reaches 0, the car stops.
+//Part 3
+//Write a method to display all the car properties (characteristics).
+
+
+
+
+        static void Main(string[] args)
+        {
+            Car MeanMachine = new Car("Ford", "Focus", "AAA123", "white");
+            MeanMachine.PrintCarInfo();
+            MeanMachine.Drive();
+            MeanMachine.PrintCarInfo();
+            MeanMachine.DriveEmpty();
+            MeanMachine.PrintCarInfo();
+        }
+    }
+}
